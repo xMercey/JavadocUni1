4a2c541c6ce4b2815bff87a77ec7023dc7cab16f
xMercey <152954454+xMercey@users.noreply.github.com>
2023-12-05 09:01:38 +0100

    Update PersonFactory.java

--- PR1AG/src/pr1/a06/PersonFactory.java
+++ PR1AG/src/pr1/a06/PersonFactory.java
@@ -1,11 +1,10 @@
 package pr1.a06;
 
 import java.io.PrintWriter;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import schimkat.berlin.lernhilfe2023ws.io.FunnyFirstFileReader;
-import schimkat.berlin.lernhilfe2023ws.io.FunnyFirstFileWriter;
 
 public class PersonFactory {
 	
@@ -17,20 +16,20 @@ public class PersonFactory {
 	
 	public static ArrayList<Person> createTestPersons() {
 		Scanner in = new Scanner(new FunnyFirstFileReader("./data/person06.txt"));
-		PrintWriter fout = new PrintWriter(new FunnyFirstFileWriter("./data/person06b.txt"));
+//		PrintWriter fout = new PrintWriter(new FunnyFirstFileWriter("./data/person06b.txt"));
 		ArrayList<Person> pList = new ArrayList<>();
 		while (in.hasNext()) {
              Person person = Person.parse(in.nextLine());
              pList.add(person);
         }
 		 
-		for (int i = 0; i < pList.size(); i++) {
-		    Person person = pList.get(i);
-		    fout.println(person.toString()); 
-		}
+//		for (int i = 0; i < pList.size(); i++) {
+//		    Person person = pList.get(i);
+//		    fout.println(person.toString()); 
+//		}
 		
 		in.close();
-		fout.close();
+//		fout.close();
 		return pList;
 	}
 }
